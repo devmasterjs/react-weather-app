d0df802cec68d8dbb8d9b9df98886979c283f820
Marlo Eichenberg Motta <devmasterjs@programmerhero.com.br>
2020-11-14 12:43:27 -0300

    Updated Test for title screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders Page Title", () => {
+test('renders Page Title', () => {
   render(<App />);
-  const titlePage = screen.getByText(/Weather Page/i);
+  const titlePage = screen.getByText(/TEMPO AGORA EM/i);
   expect(titlePage).toBeInTheDocument();
 });
